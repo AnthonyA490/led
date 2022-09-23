8d0bf886564f36933e00f658ae7e851739bda08a
AnthonyA490 <113041244+AnthonyA490@users.noreply.github.com>
2022-09-23 10:57:20 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
+function zero () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P12, 1)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+}
+input.onButtonPressed(Button.A, function () {
+    zero()
+})
 basic.showIcon(IconNames.Heart)
