6a242a14469577698a14f763ff5cc45dfab48f7e
AnthonyA490 <113041244+AnthonyA490@users.noreply.github.com>
2022-09-23 11:01:32 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ function zero () {
     pins.digitalWritePin(DigitalPin.P12, 1)
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P14, 1)
-    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P15, 1)
 }
 input.onButtonPressed(Button.A, function () {
     zero()
